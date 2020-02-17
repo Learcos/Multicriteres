ca58b1164a8a698ad159b9ee556c2db71c51d60a
Jayro5 <34172555+Jayro5@users.noreply.github.com>
2020-02-18 00:48:04 +0100

    fait jusque distillation

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 
 namespace Multicritères {
     class Program {
@@ -38,10 +37,7 @@ namespace Multicritères {
             return matriceSeuils;
         }
 
-        public static double[, ] calculeMatriceConcordance (int nbProjets, int nbCriteres) {
-            int[, ] matricePerf = initialiseMatricePerformance ();
-            int[, ] matriceSeuils = initialiseMatriceSeuils ();
-
+        public static double[, ] calculeMatriceConcordance (int[, ] matricePerf, int[, ] matriceSeuils, int nbProjets, int nbCriteres) {
             //on trouve le k
             double sommeImportances = 0.0;
             for (int a = 0; a < nbCriteres; a++) {
@@ -72,7 +68,7 @@ namespace Multicritères {
                         } else if (perfIK <= perfJK - preferenceK) {
                             concordance = 0;
                         } else {
-                            concordance = (double)(preferenceK + perfIK - perfJK) / (preferenceK - indifferenceK);
+                            concordance = (double) (preferenceK + perfIK - perfJK) / (preferenceK - indifferenceK);
                         }
                         sommeConcordances += importanceK * concordance;
                     }
@@ -83,10 +79,7 @@ namespace Multicritères {
             return matriceConcordance;
         }
 
-        public static double[, ] calculeMatriceDiscordance (int nbProjets, int nbCriteres) {
-            int[, ] matricePerf = initialiseMatricePerformance ();
-            int[, ] matriceSeuils = initialiseMatriceSeuils ();
-
+        public static double[, ] calculeMatriceDiscordance (int[, ] matricePerf, int[, ] matriceSeuils, int nbProjets, int nbCriteres) {
             //on trouve le k
             double sommeImportances = 0.0;
             for (int a = 0; a < nbCriteres; a++) {
@@ -115,7 +108,7 @@ namespace Multicritères {
                         } else if (perfIK <= perfJK - vetoK) {
                             discordance = 1;
                         } else {
-                            discordance = (double)(perfJK - perfIK - preferenceK) / (vetoK - preferenceK);
+                            discordance = (double) (perfJK - perfIK - preferenceK) / (vetoK - preferenceK);
                         }
                         sommeDiscordances += importanceK * discordance;
                     }
@@ -126,22 +119,15 @@ namespace Multicritères {
             return matriceDiscordance;
         }
 
-        public static double[, ] calculeMatriceCredibilite (int nbProjets, int nbCriteres){
-            double[, ] matriceConcordance = calculeMatriceConcordance(nbProjets, nbCriteres);
-            double[, ] matriceDiscordance = calculeMatriceDiscordance(nbProjets, nbCriteres);
-
-            //pour calculer l'ensemble J pour chaque deux projets
-            int[, ] matricePerf = initialiseMatricePerformance ();
-            int[, ] matriceSeuils = initialiseMatriceSeuils ();
-
+        public static double[, ] calculeMatriceCredibilite (double[, ] matriceConcordance, double[, ] matriceDiscordance, int[, ] matricePerf, int[, ] matriceSeuils, int nbProjets, int nbCriteres) {
             double[, ] matriceCredibilite = new double[nbProjets, nbProjets];
             //on prend tous les projets deux à deux
             for (int i = 0; i < nbProjets; i++) {
                 for (int j = 0; j < nbProjets; j++) {
-                    List<double> ensembleJ = new List<double>();
+                    List<double> ensembleJ = new List<double> ();
                     double discordance = 0.00;
                     //calcul de l'ensemble des critères discordants
-                    for(int k = 0; k < nbCriteres; k++){
+                    for (int k = 0; k < nbCriteres; k++) {
 
                         int perfIK = matricePerf[i, k];
                         int perfJK = matricePerf[j, k];
@@ -154,28 +140,120 @@ namespace Multicritères {
                         } else if (perfIK <= perfJK - vetoK) {
                             discordance = 1;
                         } else {
-                            discordance = (double)(perfJK - perfIK - preferenceK) / (vetoK - preferenceK);
+                            discordance = (double) (perfJK - perfIK - preferenceK) / (vetoK - preferenceK);
                         }
-                        if(discordance > matriceConcordance[i,j]){
-                            ensembleJ.Add(discordance);
+                        if (discordance > matriceConcordance[i, j]) {
+                            ensembleJ.Add (discordance);
                         }
                     }
-                    if(ensembleJ.Count == 0){
-                        matriceCredibilite[i,j] = matriceConcordance[i,j];
-                    }
-                    else{
+                    if (ensembleJ.Count == 0) {
+                        matriceCredibilite[i, j] = matriceConcordance[i, j];
+                    } else {
                         double cred = 1.0;
-                        foreach (double disc in ensembleJ)
-                        {
-                            cred *= (double)((1-disc)/(1-matriceConcordance[i,j]));
+                        foreach (double disc in ensembleJ) {
+                            cred *= (double) ((1 - disc) / (1 - matriceConcordance[i, j]));
                         }
-                        cred *= matriceConcordance[i,j];
-                        matriceCredibilite[i,j] = cred;
+                        cred *= matriceConcordance[i, j];
+                        matriceCredibilite[i, j] = cred;
                     }
                 }
             }
             return matriceCredibilite;
         }
+
+        public static double calculeMaxMatrice (double[, ] matrice) {
+            double max = 0.00;
+            foreach (double valeur in matrice) {
+                if (valeur == 1) return valeur;
+                if (valeur > max) max = valeur;
+            }
+            return max;
+        }
+
+        //matrice surclassement: matrice d'int car 0 ou 1
+        public static int[, ] calculeMatriceSurclassement (double[, ] matriceCredibilite, int nbProjets, double alpha = 0.3, double beta = -0.15) {
+            double maxCred = calculeMaxMatrice (matriceCredibilite);
+            double seuil = alpha + maxCred * beta;
+            double niveauCoupe = maxCred - seuil;
+            //double diff;
+            int[, ] matriceSurclassement = new int[nbProjets, nbProjets];
+            for (int i = 0; i < nbProjets; i++) {
+                for (int j = 0; j < nbProjets; j++) {
+                    //diff = Math.Abs (matriceCredibilite[i, j] - matriceCredibilite [j, i]);
+                    if (matriceCredibilite[i, j] >= niveauCoupe) {
+                        matriceSurclassement[i, j] = 1;
+                    } else {
+                        matriceSurclassement[i, j] = 0;
+                    }
+                }
+            }
+            return matriceSurclassement;
+        }
+
+        public static Dictionary<int, int> calculeQualifs (int[, ] matriceSurclassement, int nbProjets, List<int> rangsProjetsDejaClasses) {
+            Dictionary<int, int> qualifs = new Dictionary<int, int> ();
+            for (int i = 0; i < nbProjets; i++) {
+                int sommeLigne = 0, sommeColonne = 0;
+                if (!(rangsProjetsDejaClasses.Contains (i))) {
+                    for (int j = 0; j < nbProjets; j++) {
+                        if (!(rangsProjetsDejaClasses.Contains (j))) {
+                            sommeLigne += matriceSurclassement[i, j];
+                            sommeColonne += matriceSurclassement[j, i];
+                        }
+                    }
+                    qualifs.Add (i, sommeLigne - sommeColonne);
+                }
+            }
+            return qualifs;
+        }
+
+        public static int calculeQualifMinMax(Dictionary<int, int> qualifs, Func<int, int, int> function){
+            int qualifMinMax = 0;
+            foreach (KeyValuePair<int,int> item in qualifs)
+            {
+                qualifMinMax = function(qualifMinMax, item.Value);
+            }
+            return qualifMinMax;
+        }
+
+        /*public static List<List<int>> distillationAscendante (Dictionary<int, int> qualifs, int[,] matriceSurclassement, int nbProjets){
+            int qualifMin = calculeQualifMinMax(qualifs, Math.Min);
+            List<List<int>> resultat = new List<List<int>>();
+            List<int> projetsQualifMin = new List<int>();
+            List<int> rangsProjetsPasMin = new List<int>();
+            Dictionary<int, int> newQualifs = new Dictionary<int,int>();
+            foreach (KeyValuePair element in qualifs)
+            {
+                if(item.Value == qualifMin){
+                    projetsQualifMin.Add(element.Key);
+                }
+                else{
+                    rangsProjetsPasMin.Add(element.Key);
+                }
+            }
+            if(projetsQualifMin.Count > 1){
+                newQualifs = calculeQualifs(matriceSurclassement, nbProjets, rangsProjetsPasMin);
+                //on cherche à savoir si toutes les valeurs de qualification sont les mêmes
+                //auquel cas on ne distille plus les projets
+                int firstQualifValue = newQualifs.First().Value;
+                bool valeursToutesEgales = true;
+                foreach (KeyValuePair element in newQualifs)
+                {
+                    if(element.Value != firstQualifValue){
+                        valeursToutesEgales = false;
+                        break;
+                    }
+                }
+                do{
+                    return distillationAscendante(newQualifs, matriceSurclassement, nbProjets)
+                } while(!valeursToutesEgales && projetsQualifMin.Count > 0);
+            }
+            else{
+                do{
+                    return distillationAscendante(qualifs)
+                }
+            }
+        }*/
 
         public static void afficheMatrice (int[, ] matrice, int nbColonnes, int nbCriteres) {
             for (int i = 0; i < nbColonnes; i++) {
@@ -189,27 +267,40 @@ namespace Multicritères {
         public static void afficheMatrice (double[, ] matrice, int nbColonnes, int nbCriteres) {
             for (int i = 0; i < nbColonnes; i++) {
                 for (int j = 0; j < nbCriteres; j++) {
-                    Console.Write (String.Format("{0:0.##}", matrice[i, j]) + "  ");
+                    Console.Write (String.Format ("{0:0.##}", matrice[i, j]) + "  ");
                 }
                 Console.WriteLine ();
             }
         }
 
         static void Main (string[] args) {
+            int nbProjets = 5, nbCriteres = 5, nbSeuils = 4;
             int[, ] matricePerf = initialiseMatricePerformance ();
             int[, ] matriceSeuils = initialiseMatriceSeuils ();
-            double[, ] matriceConcordance = calculeMatriceConcordance (5, 5);
-            double[, ] matriceDiscordance = calculeMatriceDiscordance (5, 5);
-            double[, ] matriceCredibilite = calculeMatriceCredibilite(5,5);
-            afficheMatrice (matricePerf, 5, 5);
+            double[, ] matriceConcordance = calculeMatriceConcordance (matricePerf, matriceSeuils, nbProjets, nbCriteres);
+            double[, ] matriceDiscordance = calculeMatriceDiscordance (matricePerf, matriceSeuils, nbProjets, nbCriteres);
+            double[, ] matriceCredibilite = calculeMatriceCredibilite (matriceConcordance, matriceDiscordance, matricePerf, matriceSeuils, nbProjets, nbProjets);
+            int[, ] matriceSurclassement = calculeMatriceSurclassement (matriceCredibilite, nbProjets);
+            List<int> dejaClasses = new List<int> ();
+            Dictionary<int, int> qualifs = calculeQualifs (matriceSurclassement, nbProjets, dejaClasses);
+            afficheMatrice (matricePerf, nbProjets, nbCriteres);
             Console.WriteLine ();
-            afficheMatrice (matriceSeuils, 4, 5);
+            afficheMatrice (matriceSeuils, nbSeuils, nbCriteres);
             Console.WriteLine ();
-            afficheMatrice (matriceConcordance, 5, 5);
+            afficheMatrice (matriceConcordance, nbProjets, nbProjets);
             Console.WriteLine ();
-            afficheMatrice(matriceDiscordance, 5, 5);
+            afficheMatrice (matriceDiscordance, nbProjets, nbProjets);
             Console.WriteLine ();
-            afficheMatrice(matriceCredibilite, 5, 5);
+            afficheMatrice (matriceCredibilite, nbProjets, nbProjets);
+            Console.WriteLine ();
+            afficheMatrice (matriceSurclassement, nbProjets, nbProjets);
+            Console.WriteLine ();
+            foreach (KeyValuePair<int, int> element in qualifs) {
+                Console.WriteLine ("Projet: {0}, valeur de qualif: {1}", element.Key, element.Value);
+            }
+            Console.WriteLine ();
+            Console.WriteLine("Qualif min: " + calculeQualifMinMax(qualifs, Math.Min));
+            Console.WriteLine("Qualif max: " + calculeQualifMinMax(qualifs, Math.Max));
         }
     }
 }
